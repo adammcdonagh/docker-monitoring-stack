3b40a19abfd4c52ff983e76da0ec58b86ad74f46
Adam McDonagh <adam@elitemonkey.net>
2022-02-06 00:54:19 +0000

    test

---
+++ test.py
@@ -0,0 +1,28 @@
+def test_sum():
+    assert sum([1, 2, 3]) == 6, "Should be 6"
+
+
+def test_sum_tuple():
+    assert sum((1, 2, 2)) == 6, "Should be 6"
+
+
+if __name__ == "__main__":
+    test_sum()
+    test_sum_tuple()
+    someVariable = "sdsd"
+    colour = "red"
+    some_file = None
+    print("Everything passed")
+
+    lookup_table = {
+        "key1": "value",
+        "key2": "something",
+    }
+
+    print("as{lookup_table}dad" + lookup_table)
+
+
+# flake8-bugbear
+# pep8-naming
+# flake8-commas
+# flake8-use-fstring
